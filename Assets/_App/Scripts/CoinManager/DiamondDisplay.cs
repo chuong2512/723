b073553f6b0e2ffbf3d4aae3c092be326c0645ce
ProC <hieutg244@gmail.com>
2024-12-04 22:27:56 +0700

    sub

--- Assets/_App/Scripts/CoinManager/DiamondDisplay.cs
+++ Assets/_App/Scripts/CoinManager/DiamondDisplay.cs
@@ -14,13 +14,13 @@ public class DiamondDisplay : MonoBehaviour
 
    void Start()
    {
-      GameDataManager.Instance.playerData.onChangeDiamond += i => OnChangeDiamond(i);
+     // GameDataManager.Instance.playerData.onChangeDiamond += i => OnChangeDiamond(i);
       diamondTmp.text = $"{GameDataManager.Instance.playerData.intDiamond}";
    }
    
    void OnDestroy()
    {
-      GameDataManager.Instance.playerData.onChangeDiamond -= i => OnChangeDiamond(i);
+    //  GameDataManager.Instance.playerData.onChangeDiamond -= i => OnChangeDiamond(i);
    }
 
    private void OnChangeDiamond(int i)
